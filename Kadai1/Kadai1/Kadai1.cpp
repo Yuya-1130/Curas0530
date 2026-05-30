fd44a516a01fc97d9bd93a6d4b25dfecdf80abd9
Yuya-1130 <mtyu11.xyz@gmail.com>
2026-05-30 15:20:54 +0900

    途中

---
+++ Kadai1/Kadai1/Kadai1.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+
+
+class printStatus 
+{
+private:
+
+	std::string name_ = {};
+	int hp_ = 0;
+	int attack_ = 0;
+
+public:
+	printStatus();
+
+	printStatus(const char* name, int hp, int attack);
+};
+
+printStatus::printStatus() {
+
+	std::cout << "コンストラクタが呼ばれました" << std::endl;
+	name_ = "動物";
+	hp_ = 100;
+	attack_ = 10;
+}
+ 
+
+printStatus::printStatus(const char* name, int hp, int attack) {
+     name_ = name;
+	 hp_ = hp;
+	 attack_ = attack;
+{
+	std::cout << "コンストラクタが呼ばれました" << std::endl;
+}
+
+int main()
+{
+
+	printStatus PrintStatus1("動物", 100, 10);
+}
+
