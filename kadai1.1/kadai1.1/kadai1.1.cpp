61b77d61c8f66a6c5394d0f337dcfcc020fda59d
Yuya-1130 <ktc25a31a0059@edu.kyoto-tech.ac.jp>
2026-06-13 16:18:29 +0900

    途中

---
+++ kadai1.1/kadai1.1/kadai1.1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+class Character
+{
+private:
+    char name[10];
+    int hp;
+    int attack;
+
+public:
+  void printStatus(char*name ,int hp , int attack)
+    {
+
+    }
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
